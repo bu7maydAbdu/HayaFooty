6bf7eef5555dc910002e07febf20579916c1ca66
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2023-01-30 03:04:05 +0300

    flipping fa-angles-down and fa-angles-up

--- public/js/main.js
+++ public/js/main.js
@@ -4,16 +4,51 @@ const standingChoice = document.querySelector(".standing-choice")
 const standings = document.querySelector(".group")
 const fixuturesChoice = document.querySelector(".fixures-choice")
 const fixtures = document.querySelector(".fixures")
+const carrets = document.querySelectorAll(".carrets")
 
+// console.log(carrets)
+    
 
 standingChoice.addEventListener("click", toggleDisplay)
 
 function toggleDisplay(){
-
+   
     standings.classList.toggle("display-none")
+    // console.log(this)
+    const thisCarret = this.querySelectorAll(".carrets")
+
+    thisCarret.forEach(carret => {
+        // console.log(carret)
+      carret.classList.toggle("fa-angles-down")
+      carret.classList.toggle("fa-angles-up")
+
+    })
+    
+
+
 
 }
 
-fixuturesChoice.addEventListener("click", () => {
-    fixtures.classList.toggle("display-none")
-} )
+fixuturesChoice.addEventListener("click", displayFixtures )
+
+
+function displayFixtures (){
+  
+
+
+        fixtures.classList.toggle("display-none")
+        // console.log(this)
+        const thisCarret = this.querySelectorAll(".carrets")
+    
+        thisCarret.forEach(carret => {
+            // console.log(carret)
+            carret.classList.toggle("fa-angles-down")
+            carret.classList.toggle("fa-angles-up")
+      
+          })
+    
+    
+
+}
+
+
