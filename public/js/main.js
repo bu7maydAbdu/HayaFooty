e4880ff24e670ddf688f91937e7915b6ee9bd7cb
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2023-02-06 02:58:58 +0300

    players stats and some styling edits for the match bar

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,9 @@ const standings = document.querySelector(".group")
 const fixuturesChoice = document.querySelector(".fixures-choice")
 const fixtures = document.querySelector(".fixures")
 const carrets = document.querySelectorAll(".carrets")
+const playersStatsChoice = document.querySelector(".players-stats-choice")
+const playersStats = document.querySelector(".players-stats")
+
 
 // console.log(carrets)
     
@@ -51,4 +54,20 @@ function displayFixtures (){
 
 }
 
+playersStatsChoice.addEventListener("click", displayPlayersStats )
 
+
+
+function displayPlayersStats(){
+
+  playersStats.classList.toggle("display-none")
+  // console.log(this)
+  const thisCarret = this.querySelectorAll(".carrets")
+
+  thisCarret.forEach(carret => {
+      // console.log(carret)
+      carret.classList.toggle("fa-angles-down")
+      carret.classList.toggle("fa-angles-up")
+
+    })
+}
