9e456d1c13cf6c31d3232b66e12bab523666f4a5
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2023-03-13 03:33:57 +0300

    login process

--- routes/main.js
+++ routes/main.js
@@ -12,6 +12,8 @@ router.get("/", mainController.getHome)
 router.get("/allLeagues", mainController.getAllLeagues)
 router.get("/loginPage", accountController.getLogin)
 router.get("/signUpPage", accountController.getSignUpPage)
+router.get("/login", authController.getLogin)
+router.post("/login", authController.postLogin)
 router.get("/signup", authController.getSignup)
 router.post("/signup", authController.postSignup)
 router.get("/logout", authController.logout);
