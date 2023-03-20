2f4236944edad869d68cb4821fe7f505b14938fa
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2023-03-21 02:58:20 +0300

    favorite button

--- public/js/main.js
+++ public/js/main.js
@@ -1,129 +1,86 @@
-
-
-const standingChoice = document.querySelector(".standing-choice")
-const standings = document.querySelectorAll(".group")
-const fixuturesChoice = document.querySelector(".fixures-choice")
-const fixtures = document.querySelector(".fixures")
-const carrets = document.querySelectorAll(".carrets")
-const playersStatsChoice = document.querySelector(".players-stats-choice")
-const playersStats = document.querySelector(".players-stats")
-
-
+const standingChoice = document.querySelector(".standing-choice");
+const standings = document.querySelectorAll(".group");
+const fixuturesChoice = document.querySelector(".fixures-choice");
+const fixtures = document.querySelector(".fixures");
+const carrets = document.querySelectorAll(".carrets");
+const playersStatsChoice = document.querySelector(".players-stats-choice");
+const playersStats = document.querySelector(".players-stats");
 
 // console.log(carrets)
-    function toggleDisplay(){
-      standings.forEach(standing => {
-        // console.log(carret)
-        standing.classList.toggle("display-none")
+function toggleDisplay() {
+  standings.forEach((standing) => {
+    // console.log(carret)
+    standing.classList.toggle("display-none");
+  });
 
-
-    })
-
-    // console.log(this)
-    const thisCarret = this.querySelectorAll(".carrets")
-
-    thisCarret.forEach(carret => {
-        // console.log(carret)
-      carret.classList.toggle("fa-angles-down")
-      carret.classList.toggle("fa-angles-up")
-
-    })
-    
-
-
-
-}
-if(standingChoice){
-standingChoice.addEventListener("click", toggleDisplay)
-}
-
-
-if(fixuturesChoice){
-fixuturesChoice.addEventListener("click", displayFixtures )
-
-}
-
-
-function displayFixtures (){
-  
-
-
-        fixtures.classList.toggle("display-none")
-        // console.log(this)
-        const thisCarret = this.querySelectorAll(".carrets")
-    
-        thisCarret.forEach(carret => {
-            // console.log(carret)
-            carret.classList.toggle("fa-angles-down")
-            carret.classList.toggle("fa-angles-up")
-      
-          })
-    
-    
-
-}
-
-
-
-if(playersStatsChoice){
-playersStatsChoice.addEventListener("click", displayPlayersStats )
-}
-
-
-
-function displayPlayersStats(){
-
-  playersStats.classList.toggle("display-none")
   // console.log(this)
-  const thisCarret = this.querySelectorAll(".carrets")
+  const thisCarret = this.querySelectorAll(".carrets");
 
-  thisCarret.forEach(carret => {
-      // console.log(carret)
-      carret.classList.toggle("fa-angles-down")
-      carret.classList.toggle("fa-angles-up")
-
-    })
+  thisCarret.forEach((carret) => {
+    // console.log(carret)
+    carret.classList.toggle("fa-angles-down");
+    carret.classList.toggle("fa-angles-up");
+  });
+}
+if (standingChoice) {
+  standingChoice.addEventListener("click", toggleDisplay);
 }
 
-
-
-
-// sidebar section 
-const sidebarButton =  document.querySelector(".fa-bars")
-
- 
-sidebarButton.addEventListener("click",()=>{
-
-if(sidebarButton){
-  console.log(sidebarButton)
-}else {
-console.log("btn not working")
+if (fixuturesChoice) {
+  fixuturesChoice.addEventListener("click", displayFixtures);
 }
 
-})
+function displayFixtures() {
+  fixtures.classList.toggle("display-none");
+  // console.log(this)
+  const thisCarret = this.querySelectorAll(".carrets");
+
+  thisCarret.forEach((carret) => {
+    // console.log(carret)
+    carret.classList.toggle("fa-angles-down");
+    carret.classList.toggle("fa-angles-up");
+  });
+}
+
+if (playersStatsChoice) {
+  playersStatsChoice.addEventListener("click", displayPlayersStats);
+}
+
+function displayPlayersStats() {
+  playersStats.classList.toggle("display-none");
+  // console.log(this)
+  const thisCarret = this.querySelectorAll(".carrets");
+
+  thisCarret.forEach((carret) => {
+    // console.log(carret)
+    carret.classList.toggle("fa-angles-down");
+    carret.classList.toggle("fa-angles-up");
+  });
+}
+
+// sidebar section
+const sidebarButton = document.querySelector(".fa-bars");
+
+sidebarButton.addEventListener("click", () => {
+  if (sidebarButton) {
+    console.log(sidebarButton);
+  } else {
+    console.log("btn not working");
+  }
+});
 
 // sideBarOpener()
 
-
-
-
-
-
-
-
-function toggleSideBar(){
-const sidebar = document.querySelector(".sidebar")
-console.log(sidebar)
-console.log("hi")
-if(sidebar){
-sidebar.classList.toggle("open-active")
-
-}else {
-console.log("not reached")
+function toggleSideBar() {
+  const sidebar = document.querySelector(".sidebar");
+  console.log(sidebar);
+  console.log("hi");
+  if (sidebar) {
+    sidebar.classList.toggle("open-active");
+  } else {
+    console.log("not reached");
+  }
 }
-}
-
-
 
 // function toggleActive(e){
 //   console.log(e.propertyName)
