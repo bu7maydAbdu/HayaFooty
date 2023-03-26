21f9e69a16db20dd7270c2ea67af7b9bf392cee1
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2023-03-26 05:13:48 +0300

    finding favLeagues set

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const ProfileSet = require("../models/ProfileSet");
+const axios = require("axios");
+
+const token = process.env.token;
+const baseUrl = process.env.baseUrl;
+const standingsUrl = `${baseUrl}/2000/standings`;
+const teamsUrl = `${baseUrl}/teams`;
+const matchesUrl = `${baseUrl}/matches`;
 
 module.exports = {
   getProfile: async (req, res) => {
     try {
+      const profileSettings = await ProfileSet.findOne({ user: req.user.id });
+      const favArr = profileSettings.favoriteLeagues;
+      for (let i = 0; i < favArr.length; i++) {}
+
       res.render("profile.ejs", { myProfileInfo: req.user });
       console.log(req.user);
     } catch (err) {
